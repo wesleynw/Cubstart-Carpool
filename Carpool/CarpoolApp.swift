68220cd8004506111a28dbc967db6855a1e81d50
Wesley Weisenberger <wesley@berkeley.edu>
2022-11-29 16:03:16 -0800

    initial commit

---
+++ Carpool/CarpoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarpoolApp.swift
+//  Carpool
+//
+//  Created by Wesley Weisenberger on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CarpoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
